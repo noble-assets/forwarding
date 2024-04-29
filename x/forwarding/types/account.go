dad73a5c46c4edfca4bf507e21f6ab1b6a172552
John Letey <john@nobleassets.xyz>
2024-04-29 11:42:05 +0200

    fix: implement genesis account interface

--- x/forwarding/types/account.go
+++ x/forwarding/types/account.go
@@ -1,11 +1,32 @@
 package types
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/address"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	channeltypes "github.com/cosmos/ibc-go/v8/modules/core/04-channel/types"
+)
+
+var (
+	_ sdk.AccountI             = &ForwardingAccount{}
+	_ authtypes.GenesisAccount = &ForwardingAccount{}
 )
 
 func GenerateAddress(channel string, recipient string) sdk.AccAddress {
 	bz := []byte(channel + recipient)
 	return address.Derive([]byte(ModuleName), bz)[12:]
+}
+
+func (fa *ForwardingAccount) Validate() error {
+	if !channeltypes.IsValidChannelID(fa.Channel) {
+		return fmt.Errorf("channel id (%s) is invalid for account %s", fa.Channel, fa.Address)
+	}
+
+	if fa.CreatedAt < 0 {
+		return fmt.Errorf("%d is an invalid creation height for account %s", fa.CreatedAt, fa.Address)
+	}
+
+	return fa.BaseAccount.Validate()
 }
