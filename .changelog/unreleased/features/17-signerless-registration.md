6bfe328fbd9d4ab8811c9df302812fe59e4b542d
John Letey <john@noble.xyz>
2024-08-29 19:42:52 +0200

    feat: allow signerless account registration (#16)

---
+++ .changelog/unreleased/features/17-signerless-registration.md
@@ -0,0 +1,1 @@
+- Allow accounts with a balance to be registered signerlessly. ([#17](https://github.com/noble-assets/forwarding/pull/17))
--- x/forwarding/ante.go
+++ x/forwarding/ante.go
@@ -3,8 +3,12 @@ package forwarding
 import (
 	cctptypes "github.com/circlefin/noble-cctp/x/cctp/types"
 	fiattokenfactorytypes "github.com/circlefin/noble-fiattokenfactory/x/fiattokenfactory/types"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
 	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/cosmos/cosmos-sdk/x/authz"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/noble-assets/forwarding/x/forwarding/keeper"
@@ -119,4 +123,53 @@ func (d Decorator) CheckMessages(ctx sdk.Context, msgs []sdk.Msg) error {
 	}
 
 	return nil
+}
+
+//
+
+func SigVerificationGasConsumer(
+	meter storetypes.GasMeter, sig signing.SignatureV2, params authtypes.Params,
+) error {
+	switch sig.PubKey.(type) {
+	case *types.ForwardingPubKey:
+		return nil
+	default:
+		return ante.DefaultSigVerificationGasConsumer(meter, sig, params)
+	}
+}
+
+//
+
+type SigVerificationDecorator struct {
+	underlying ante.SigVerificationDecorator
+	bank       types.BankKeeper
+}
+
+var _ sdk.AnteDecorator = SigVerificationDecorator{}
+
+func NewSigVerificationDecorator(ak ante.AccountKeeper, bk types.BankKeeper, signModeHandler authsigning.SignModeHandler) SigVerificationDecorator {
+	return SigVerificationDecorator{
+		underlying: ante.NewSigVerificationDecorator(ak, signModeHandler),
+		bank:       bk,
+	}
+}
+
+func (d SigVerificationDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
+	if msgs := tx.GetMsgs(); len(msgs) == 1 {
+		msg, ok := msgs[0].(*types.MsgRegisterAccount)
+		if !ok {
+			return d.underlying.AnteHandle(ctx, tx, simulate, next)
+		}
+
+		address := types.GenerateAddress(msg.Channel, msg.Recipient)
+		balance := d.bank.GetAllBalances(ctx, address)
+
+		if balance.IsZero() || msg.Signer != address.String() {
+			return d.underlying.AnteHandle(ctx, tx, simulate, next)
+		}
+
+		return next(ctx, tx, simulate)
+	}
+
+	return d.underlying.AnteHandle(ctx, tx, simulate, next)
 }
